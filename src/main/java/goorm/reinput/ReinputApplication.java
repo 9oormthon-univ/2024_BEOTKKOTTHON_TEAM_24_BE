350176c43756486424142302cb7f4fd9cc6a9f4f
Jhsysng <ysng1118@naver.com>
2024-03-16 16:14:47 +0900

    init: spring initialize

---
+++ src/main/java/goorm/reinput/ReinputApplication.java
@@ -0,0 +1,13 @@
+package goorm.reinput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReinputApplication.class, args);
+    }
+
+}
---
+++ src/test/java/goorm/reinput/ReinputApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.reinput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
