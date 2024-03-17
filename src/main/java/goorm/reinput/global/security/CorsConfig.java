f7479208c31f992d3dba9be01a741affefda178a
MinhoJJang <ahchjang@naver.com>
2024-03-17 16:05:54 +0900

    chore: default swagger setting
    swagger에 접속할 수 있게 기본적인 세팅을 완료했습니다. 추가적으로 CorsConfig, WebSecurityConfig 도 기본 세팅만 완료했습니다.
    
    resolve: #2

---
+++ src/main/java/goorm/reinput/global/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package goorm.reinput.global.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    // TODO: cors 설정. dev, prod 주소 추가 예정
+//    @Value("${cors.origin.development}")
+//    private String developmentOrigin;
+//
+//    @Value("${cors.origin.production}")
+//    private String productionOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("OPTIONS", "HEAD", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:3000");
+    }
+}
---
+++ src/main/java/goorm/reinput/global/swagger/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package goorm.reinput.global.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(info = @Info(
+        title = "Reinput API",
+        description = "2024_BEOTKKOTTHON_TEAM_24_Reinput API",
+        version = "v1.0.0"))
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openApi() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
