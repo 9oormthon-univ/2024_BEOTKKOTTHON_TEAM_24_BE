b7478740350a190919a9a14106fbfe7f0fa6dff4
Jhsysng <ysng1118@naver.com>
2024-03-19 18:00:16 +0900

    feat: principa details 추가

---
+++ src/main/java/goorm/reinput/global/auth/PrincipalDetails.java
@@ -0,0 +1,64 @@
+package goorm.reinput.global.auth;
+
+import goorm.reinput.user.domain.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Data
+public class PrincipalDetails implements UserDetails {
+    private User user;
+    private Map<String, Object> attributes;
+
+    //normal login
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    //oauth login
+    /*
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }*/
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getUserPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnable();
+    }
+}
