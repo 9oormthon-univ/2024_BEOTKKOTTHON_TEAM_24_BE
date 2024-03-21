386525db6fe215af8326d968fa9ce75c6280c31c
MinhoJJang <ahchjang@naver.com>
2024-03-21 19:51:33 +0900

    feat: saveInsight * 일부 구현완료 * Reminder 관련 service 생성 시 추가 예정

---
+++ src/main/java/goorm/reinput/insight/controller/InsightController.java
@@ -0,0 +1,47 @@
+package goorm.reinput.insight.controller;
+
+import goorm.reinput.insight.domain.dto.InsightCreateDto;
+import goorm.reinput.insight.service.InsightService;
+import goorm.reinput.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/insight")
+public class InsightController {
+
+    private final InsightService insightService;
+    private final UserService userService;
+
+    @Operation(summary = "인사이트 저장", description = "유저가 인사이트를 등록할 때 사용하는 API 입니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201"),
+            @ApiResponse(responseCode = "401"),
+            @ApiResponse(responseCode = "403"),
+            @ApiResponse(responseCode = "500")
+    })
+    @PostMapping()
+    public ResponseEntity<InsightCreateDto> saveInsight(@Parameter(hidden = true) final @AuthenticationPrincipal Long userId, final @Valid @RequestBody InsightCreateDto insightCreateDto) {
+        log.info("[InsightController] storeInsight {} called", userId);
+        insightService.saveInsight(userId, insightCreateDto);
+
+        // 인사이트 리스트 반환
+        return
+    }
+
+    // 2. 인사이트 상세보기 GET
+
+
+    // 3. 인사이트 전체 검색 GET
+
+}
--- src/main/java/goorm/reinput/insight/service/InsightService.java
+++ src/main/java/goorm/reinput/insight/service/InsightService.java
@@ -1,11 +1,24 @@
 package goorm.reinput.insight.service;
 
-import goorm.reinput.insight.domain.dto.InsightRequestDto;
+import goorm.reinput.folder.domain.Folder;
+import goorm.reinput.folder.domain.FolderColor;
+import goorm.reinput.folder.repository.FolderRepository;
+import goorm.reinput.folder.service.FolderService;
+import goorm.reinput.insight.domain.HashTag;
+import goorm.reinput.insight.domain.Insight;
+import goorm.reinput.insight.domain.InsightImage;
+import goorm.reinput.insight.domain.dto.InsightCreateDto;
+import goorm.reinput.insight.domain.repository.HashTagRepository;
+import goorm.reinput.insight.domain.repository.InsightImageRepository;
 import goorm.reinput.insight.repository.InsightRepository;
 import goorm.reinput.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -13,8 +26,63 @@ import org.springframework.stereotype.Service;
 public class InsightService {
     private final InsightRepository insightRepository;
     private final UserRepository userRepository;
+    private final FolderRepository folderRepository;
+    private final InsightImageRepository insightImageRepository;
+    private final HashTagRepository hashTagRepository;
+    private final FolderService folderService;
+    @Transactional
+    public void saveInsight(Long userId, InsightCreateDto dto){
 
-    public InsightRequestDto insightStore(InsightRequestDto dto){
-        return dto;
+        // 1. folderName이 기존애 존재하지 않을 경우, 먼저 폴더를 생성합니다.
+        Optional<Folder> folderOptional = folderRepository.findByFolderName(dto.getFolderName());
+        if(!folderOptional.isPresent()) {
+            folderService.saveFolder(userId, dto.getFolderName(), FolderColor.BLUE);
+        }
+
+        // 2. folderName이 존재할 경우, builder()을 사용해 Insight을 만들고 save 합니다.
+        Insight insight = Insight.builder()
+                .folder(folderRepository.findByFolderName(dto.getFolderName()).orElseThrow(() -> {
+                    log.error("[InsightService] folder not found by folderName");
+                    return new IllegalArgumentException("folder not found");
+                }))
+                .insightUrl(dto.getInsightUrl())
+                .insightMemo(dto.getInsightMemo())
+                .insightSource(dto.getInsightSource())
+                .insightTitle(dto.getInsightTitle())
+                .insightSummary(dto.getInsightSummary())
+                .insightMainImage(dto.getInsightMainImage())
+                .viewCount(0)
+                .build();
+
+        insightRepository.save(insight);
+
+        // 3. 사진 리스트를 저장합니다.
+        List<InsightImage> insightImageList = dto.getInsightImageList();
+        for(InsightImage image : insightImageList){
+            InsightImage insightImage = InsightImage.builder()
+                    .insightImageUrl(image.getInsightImageUrl())
+                    .build();
+
+            insightImageRepository.save(insightImage);
+        }
+
+        // 4. 해시태그 리스트를 저장합니다.
+        List<HashTag> hashTagList = dto.getHashTagList();
+        for (HashTag tag : hashTagList){
+            HashTag hashTag = HashTag.builder()
+                    .hashTagName(tag.getHashTagName())
+                    .build();
+
+            hashTagRepository.save(hashTag);
+        }
+
+        // TODO. 5. reminder을 생성합니다.
+
+
+        // TODO. 6. reminder-date를 생성합니다.
+
+
+        // TODO. 7. reminder-question을 생성합니다.
+
     }
 }
