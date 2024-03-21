4fff114a8be3035940ca08b6fb24806044358ca4
Jhsysng <ysng1118@naver.com>
2024-03-21 17:50:12 +0900

    feat: insight 도메인 repository추가

---
+++ src/main/java/goorm/reinput/insight/domain/repository/HashTagRepository.java
@@ -0,0 +1,9 @@
+package goorm.reinput.insight.domain.repository;
+
+import goorm.reinput.insight.domain.HashTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HashTagRepository extends JpaRepository<HashTag, Long>{
+}
