8e3ac7f59f9ac6aca548ad10d028d34df5d20b9c
Jhsysng <ysng1118@naver.com>
2024-05-02 15:00:26 +0900

    feat: global exception

---
+++ src/main/java/goorm/reinput/global/controller/GlobalControllerAdvice.java
@@ -0,0 +1,10 @@
+package goorm.reinput.global.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
+
+}
