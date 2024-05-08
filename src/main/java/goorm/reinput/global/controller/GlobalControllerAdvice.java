02b7f5dcd9e797e93257668be8d29af1e4c739f1
Jhsysng <ysng1118@naver.com>
2024-05-08 19:05:50 +0900

    feat: folder exception 및 valid global ControllerAdvice 적용

--- src/main/java/goorm/reinput/global/controller/GlobalControllerAdvice.java
+++ src/main/java/goorm/reinput/global/controller/GlobalControllerAdvice.java
@@ -7,18 +7,34 @@ import goorm.reinput.user.exception.CustomUserException;
 import goorm.reinput.user.exception.UserErrorCode;
 import jdk.jshell.spi.ExecutionControl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
+    /**
+     * exception : ValidationEx001
+     * category : MethodArgumentNotValidException
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponseDto("ValidationEx001", e.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
+    }
+    /**
+     * exception : UserDomainException
+     * category : CustomUserException
+     */
     @ExceptionHandler(CustomUserException.class)
     public ResponseEntity<ErrorResponseDto> handleUserException(CustomUserException e) {
         final UserErrorCode errorCode = e.getErrorCode();
         return ResponseEntity.badRequest().body(new ErrorResponseDto(errorCode.getCode(), errorCode.getMessage()));
     }
-
+    /**
+     * exception : FolderDomainException
+     * category : CustomFolderException
+     */
     @ExceptionHandler(CustomFolderException.class)
     public ResponseEntity<ErrorResponseDto> handleFolderException(CustomFolderException e) {
         final FolderErrorCode errorCode = e.getErrorCode();
