7d85f3723fed54ad7a691ba620088660505f2266
MinhoJJang <ahchjang@naver.com>
2024-05-29 17:37:01 +0900

    feat: 유저 정보 반환 api 생성
    token만 있다면 언제든 유저 정보 (이름, 이메일, 직업) 를 가져올 수 있는 api를 생성했습니다.

--- src/main/java/goorm/reinput/user/controller/UserController.java
+++ src/main/java/goorm/reinput/user/controller/UserController.java
@@ -1,10 +1,7 @@
 package goorm.reinput.user.controller;
 
 import goorm.reinput.global.auth.PrincipalDetails;
-import goorm.reinput.user.domain.dto.TokenRequest;
-import goorm.reinput.user.domain.dto.TokenResponseDto;
-import goorm.reinput.user.domain.dto.UserLoginDto;
-import goorm.reinput.user.domain.dto.UserSignupDto;
+import goorm.reinput.user.domain.dto.*;
 import goorm.reinput.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -26,6 +23,7 @@ public class UserController {
     public String healthcheck() {
         return "OK";
     }
+
     @Operation(summary = "회원가입", description = "유저가 회원가입할 때 사용하는 API")
     @PostMapping("/signup")
     public ResponseEntity<String> signUp(final @Valid @RequestBody UserSignupDto userSignupDto) {
@@ -35,26 +33,36 @@ public class UserController {
 
         return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
+
     @Operation(summary = "로그인", description = "유저가 로그인할 때 사용하는 API")
     @PostMapping("/login")
-    public ResponseEntity<TokenResponseDto> login(final @Valid @RequestBody UserLoginDto userLoginDto){
+    public ResponseEntity<TokenResponseDto> login(final @Valid @RequestBody UserLoginDto userLoginDto) {
         log.info("[UserController] login userEmail : {}", userLoginDto.getUserEmail());
         return ResponseEntity.ok().body(userService.login(userLoginDto));
     }
+
     @Operation(summary = "토큰 재발급", description = "유저가 토큰을 재발급할 때 사용하는 API")
     @PostMapping("/refresh")
     public ResponseEntity<TokenResponseDto> refresh(final @RequestBody TokenRequest refreshToken) {
         return ResponseEntity.ok().body(userService.reissueAccessToken(refreshToken.getRefreshToken()));
     }
+
     @Operation(summary = "회원탈퇴", description = "유저가 회원탈퇴할 때 사용하는 API")
     @DeleteMapping("deactivate")
     public ResponseEntity<String> deactivateUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         userService.deactivate(principalDetails.getUserId());
         return new ResponseEntity<>("success", HttpStatus.OK);
     }
+
     @Operation(summary = "로그아웃", description = "유저가 로그아웃할 때 사용하는 API 현재는 아무런 작업을 하지 않음, 프론트에서 토큰 삭제 처리")
     @DeleteMapping("/logout")
     public ResponseEntity<String> logout() {
         return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @Operation(summary = "유저 정보 가져오기", description = "유저 정보(이름, 직업, 이메일)가 필요할 때 호출하는 API")
+    @GetMapping("/info")
+    public ResponseEntity<UserInfoDto> getUserInfo(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        return ResponseEntity.ok().body(userService.getUserInfo(principalDetails.getUserId()));
     }
 }
